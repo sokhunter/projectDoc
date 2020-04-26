b6a38b8d999c8c747e58a8728c105c426b1e932b
R00t_5layer <30440872+R00t5layer@users.noreply.github.com>
2020-04-26 17:25:38 -0500

    Entidades de la DB

---
+++ Doctor.Entities/Cita.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Doctor.Entities
+{
+    public class Cita
+    {
+        public int CitaId { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Tamaño entre 3 a 50 caracteres")]
+        public string Motivo { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tamaño entre 3 a 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tamaño entre 3 a 100 caracteres")]
+        public string Sintomas { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tamaño entre 3 a 100 caracteres")]
+        public string Exploracion { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tamaño entre 3 a 100 caracteres")]
+        public string Indicacion { get; set; }
+        public int PacienteId { get; set; }
+        public int DoctorId { get; set; }
+
+
+        public virtual Paciente Paciente { get; set; }
+
+        public virtual Medico Doctor { get; set; }
+
+
+
+        public virtual ICollection<CitaExamen> CitaExamen { get; set; }
+        public virtual ICollection<Diagnostico> Diagnostico { get; set; }
+        public virtual ICollection<Receta> Receta { get; set; }
+    }
+}
---
+++ Doctor.Entities/Enfermedad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Doctor.Entities
+{
+    public class Enfermedad
+    {
+        public int EnfermedadId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Diagnostico> Diagnostico { get; set; }
+    }
+}
